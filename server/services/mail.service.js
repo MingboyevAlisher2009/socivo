4bb6fde9491907faf712ef351439d5357fa9f360
MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2025-12-15 00:04:06 +0500

    smtp issue

--- server/services/mail.service.js
+++ server/services/mail.service.js
@@ -13,6 +13,9 @@ class MailService {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
   }
 
