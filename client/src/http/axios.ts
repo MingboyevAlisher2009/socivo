04b7432e195e318ddbbbf63d4bdde6cfee1c096d
MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2025-12-14 23:13:50 +0500

    base url issue

--- client/src/http/axios.ts
+++ client/src/http/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/" : "/"
 
 const axiosInstance = axios.create({
     baseURL: `${BASE_URL}/api`,
