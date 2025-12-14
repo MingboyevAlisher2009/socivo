ecf8e63506c664986280003eb4938688377c6ebd
MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2025-12-15 01:18:24 +0500

    some changes

--- client/src/http/axios.ts
+++ client/src/http/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : "https://socivo-1rwf.onrender.com"
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
     baseURL: `${BASE_URL}/api`,
