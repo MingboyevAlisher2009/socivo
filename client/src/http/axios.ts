314937a48b97d5540784b92ebe1d1f18d26b12a8
MingboyevAlisher2009 <mingboyevalisher171@gmail.com>
2025-12-14 23:32:57 +0500

    base url

--- client/src/http/axios.ts
+++ client/src/http/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/" : "https://socivo-7kxk.vercel.app"
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/" : "https://socivo-1rwf.onrender.com"
 
 const axiosInstance = axios.create({
     baseURL: `${BASE_URL}/api`,
